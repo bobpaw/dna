7dad1649b61ac16aad2ee60e1a72d6fa95c40acb
Aiden Woodruff <aiden.woodruff@gmail.com>
2019-03-18 14:06:14 -0500

    Added letters and name functions
    name prints the corresponding amino acid short name
    letters prints out the single letter code for the amino
    Removed codons.info
    Added equality and inequality operators
    Signed char instead of unsigned
    
    Signed-off-by: Aiden Woodruff <aiden.woodruff@gmail.com>

--- dna.cc
+++ dna.cc
@@ -3,8 +3,6 @@
 #include "dna.h"
 
 int main () {
-	std::cout << (std::string) DNA::Met << std::endl;
-	std::cout << (std::string) DNA::Codon(DNA::RNA, DNA::C, DNA::G, DNA::A) << std::endl;
-	std::cout << DNA::Met() << std::endl;
+	std::cout << DNA::Met.name() << std::endl;
 	return 0;
 }
--- dna.h
+++ dna.h
@@ -1,39 +1,56 @@
 #include <string>
+#include <type_traits>
 
 #ifndef DNA_DNA_H_
 #define DNA_DNA_H_
 
 namespace DNA {
-	const unsigned char A = 0, T = 1, U = T, C = 2, G = 3, DNA = 0, RNA = 1;
+	const char letters[65] = "KOQEILLVTSPARORGNYHDIFLVTSPASCRGNYHDIFLVTSPASCRGKOQEMLLVTSPARWRG";
+
+	const char names[64][4] = {"Lys", "Stp", "Gln", "Glu", "Ile", "Leu", "Leu",
+	"Val", "Thr", "Ser", "Pro", "Ala", "Arg", "Stp", "Arg", "Gly", "Asn", "Tyr",
+	"His", "Asp", "Ile", "Phe", "Leu", "Val", "Thr", "Ser", "Pro", "Ala", "Ser",
+	"Cys", "Arg", "Gly", "Asn", "Tyr", "His", "Asp", "Ile", "Phe", "Leu", "Val",
+	"Thr", "Ser", "Pro", "Ala", "Ser", "Cys", "Arg", "Gly", "Lys", "Stp", "Gln",
+	"Glu", "Met", "Leu", "Leu", "Val", "Thr", "Ser", "Pro", "Ala", "Arg", "Trp",
+	"Arg", "Gly"};
+
+	const char A = 0, T = 1, U = T, C = 2, G = 3, DNA = 0, RNA = 1;
 	class Codon {
 	private:
-		unsigned char bm;
+		char bm;
 	public:
-		Codon (unsigned char t, unsigned char b1, unsigned char b2, unsigned char b3) {
+		Codon (decltype(bm) t, decltype(bm) b1, decltype(bm) b2, decltype(bm) b3) {
 			setType(t);
 			set(0, b1);
 			set(1, b2);
 			set(2, b3);
 		}
 
-		Codon (unsigned char b) : bm(b) {}
+		Codon (decltype(bm) b) : bm(b) {}
 
-		unsigned char operator[] (int n) const {
+		unsigned char operator[] (int n) const noexcept {
 			return (bm >> n * 2) & 0x3;
 		}
 
-		unsigned char operator() () const { return bm; }
+		decltype(bm) operator() () const noexcept { return bm; }
 
-		void set (int n, unsigned char val) {
+		decltype(letters[0]) letter (void) const { return letters[(int) (bm & 63)]; }
+		decltype(names[0]) name (void) const { return names[(int) (bm & 63)]; }
+
+		void set (int n, decltype(bm) val) noexcept {
 			val &= 0x3;
 			val <<= n * 2;
 			bm &= ~(0x3 << n * 2);
 			bm |= val;
 		}
 
-		void setType (unsigned char t) {
-			bm &= ~(64);
-			bm |= t & 1 == 1 ? 64 : 0;
+		void setType (decltype(DNA) t) noexcept {
+			if (t & 1 == 0) {
+				bm &= ~(64);
+			} else {
+				bm |= 64;
+			}
 		}
 
 		operator std::string () const {
@@ -58,6 +75,15 @@ namespace DNA {
 			}
 			return ret;
 		}
+
+		bool operator== (const Codon &other) const noexcept {
+			return bm == other.bm;
+		}
+
+		bool operator!= (const Codon &other) const noexcept {
+			return !operator==(other);
+		}
+
 	} Met(RNA, A, U, G); // class Codon
 } // namespace DNA
 
